d53cba643b278b00a47d981b2ff980e751e0df36
Westerbly Snaydley <westerbly@gmail.com>
2022-07-07 04:35:09 -0300

    fix bootstrap script on windows

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
+if [ -z ${EO_URL+x} ]; then
+    EO_URL="https://gist.githubusercontent.com/radgeRayden/e1dca7981335254f750d50fae6943645/raw/9883f61aa57ece45f78a3220d2bb1a94a93cc9d3/eo";
+fi
+
 EO=$(mktemp)
-wget "https://hg.sr.ht/~duangle/majoreo/raw/eo?rev=tip" -O $EO
+wget $EO_URL -O $EO
 chmod +x $EO
 
-echo "y" | $EO import "https://raw.githubusercontent.com/ScopesCommunity/eo-packages/main/scopes-community.eo"
+#clean everytyhing
+rm -rf .eo
+rm -rf ./lib
+rm -rf ./recipes
+
+echo "y" | $EO init "https://raw.githubusercontent.com/ScopesCommunity/eo-packages/main/scopes-community.eo"
 $EO import sdl2
 $EO import wgpu
 $EO import stb
